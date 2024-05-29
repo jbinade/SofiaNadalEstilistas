85843eaee4471a5cf5c4591fee44a661db4e7f7e
jbinade <jbinade11@hotmail.com>
2024-05-29 18:54:31 +0200

    arreglo error charge

--- CreateCharge.php
+++ CreateCharge.php
@@ -9,19 +9,6 @@ $hora = $_POST["hora"];
 $dni = $_SESSION["dni"];
 $pagado = "SI";
 
-$con = new Conexion();
-$conexion = $con->conectar_db();
-
-$stmt = $conexion->prepare("SELECT * FROM clientes WHERE dni = :dni");
-$stmt->bindParam(':dni', $dni, PDO::PARAM_STR);
-$stmt->execute();
-$res = $stmt->fetch(PDO::FETCH_OBJ);
-
-$stmtServicio = $conexion->prepare("SELECT * FROM servicios WHERE codigo = :codigo");
-$stmtServicio->bindParam(':codigo', $servicio, PDO::PARAM_STR);
-$stmtServicio->execute();
-$res = $stmtServicio->fetch(PDO::FETCH_OBJ);
-
 // Convertir el monto a centavos y asegurarse de que sea un entero
 $precioEnCentimos = intval($precio * 100);
 
@@ -35,7 +22,7 @@ $precioEnCentimos = intval($precio * 100);
   $charge = \Stripe\Charge::create([
     "amount" => $precioEnCentimos,
     "currency" => "eur",
-    "description" => "Reserva servicio " . $resServicio->nombre . " por cliente con DNI: " . $dni,
+    "description" => "Reserva servicio " . $servicio . " por cliente con DNI: " . $dni,
     "source" => $token
   ]);
 
